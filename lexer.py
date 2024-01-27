2b06b352e55f908b1e9b48e886c8d0755ad504f7
Tanner-01090 <Tanner-01090@hotmail.com>
2024-01-28 07:38:29 +1100

    Added Lexer.py

---
+++ lexer.py
@@ -0,0 +1,46 @@
+"""
+let five = 5; 
+let ten = 10; 
+
+let add = fn( x, y) {
+    x + y; 
+}; 
+
+let result = add( five, ten);
+
+"""
+
+# TOKENS
+
+ILLEGAL = "ILLEGAL"
+EOF = "EOF"
+
+# Identifiers + literals
+IDENT = "IDENT" # add, x, y
+INT = "INT"
+
+# Operators
+ASSIGN = "="
+PLUS = "+"
+
+# Delimiters
+COMMA = ","
+SEMICOLON = ";"
+
+LPAREN = "("
+RPAREN = ")"
+LBRACE = "{"
+RBRACE = "}"
+
+# Keywords
+FUNCTION = "FUNCTION"
+LET = "LET"
+
+class Token:
+    token_type = ""
+    literal = ""
+
+    def __init__(self, token_type, literal):
+        self.token_type = token_type
+        self.literal = literal
+    
